106c129ce7572a42ffe0b33483e42ef6f158a3ec
Steve High <steve.high@gmail.com>
2020-05-19 17:53:59 -0400

    moved slices executable

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -39,12 +39,12 @@ func BenchmarkUserSlice_Map(b *testing.B) {
 	benchmarks := []struct {
 		name    string
 		slice   []User
-		mapFunc func(User) User
+		mapFunc func(*User) *User
 	}{
 		{
 			name:  "10 users",
 			slice: makeUsers(10),
-			mapFunc: func(user User) User {
+			mapFunc: func(user *User) *User {
 				user.Name = "woot"
 				user.Age = 36
 
@@ -54,7 +54,7 @@ func BenchmarkUserSlice_Map(b *testing.B) {
 		{
 			name:  "100 users",
 			slice: makeUsers(100),
-			mapFunc: func(user User) User {
+			mapFunc: func(user *User) *User {
 				user.Name = "woot"
 				user.Age = 36
 
@@ -64,7 +64,7 @@ func BenchmarkUserSlice_Map(b *testing.B) {
 		{
 			name:  "1000 users",
 			slice: makeUsers(1000),
-			mapFunc: func(user User) User {
+			mapFunc: func(user *User) *User {
 				user.Name = "woot"
 				user.Age = 36
 
@@ -74,7 +74,7 @@ func BenchmarkUserSlice_Map(b *testing.B) {
 		{
 			name:  "10000 users",
 			slice: makeUsers(10000),
-			mapFunc: func(user User) User {
+			mapFunc: func(user *User) *User {
 				user.Name = "woot"
 				user.Age = 36
 
@@ -84,7 +84,7 @@ func BenchmarkUserSlice_Map(b *testing.B) {
 		{
 			name:  "100000 users",
 			slice: makeUsers(100000),
-			mapFunc: func(user User) User {
+			mapFunc: func(user *User) *User {
 				user.Name = "woot"
 				user.Age = 36
 
